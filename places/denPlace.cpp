f1652c26f3bdc00572ff09e84c16d447672bb926
Varptelion <maikl.1997@mail.ru>
2020-05-29 10:26:07 +0400

    Added Denplaces + redacted map

--- places/denPlace.cpp
+++ places/denPlace.cpp
@@ -2,5 +2,19 @@
 
 DenPlace::DenPlace()
 {
+    sprite = new QPixmap(":/res/images/Spawn.png");
+}
 
+QRectF DenPlace::boundingRect() const
+{
+    return QRectF(0, 0, sprite->width(), sprite->height());
+}
+
+void DenPlace::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
+{
+    painter->setBrush(QBrush(*sprite));
+    painter->setPen(Qt::NoPen);
+    painter->drawRect(0, 0, sprite->width(), sprite->height());
+    Q_UNUSED(option);
+    Q_UNUSED(widget);
 }
--- places/denPlace.h
+++ places/denPlace.h
@@ -1,11 +1,16 @@
-#ifndef DENPLACE_H
-#define DENPLACE_H
+#pragma once
+#include <QGraphicsObject>
+#include <QPainter>
 
-
-class DenPlace
+class DenPlace : public QGraphicsObject
 {
+    Q_OBJECT
+
 public:
     DenPlace();
+protected:
+    QRectF boundingRect() const;
+    void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget);
+private:
+    QPixmap * sprite;
 };
-
-#endif // DENPLACE_H
